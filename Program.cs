971fd470df18e036d750121cd44b7263526c90e1
Trey Suiter <trey.suiter@gmail.com>
2020-04-06 10:28:30 -0500

    test

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+        }
+    }
+}
