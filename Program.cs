494916d0a2330ded46a251f1029fb2ef8a29f9f5
Trey Suiter <trey.suiter@gmail.com>
2020-04-07 08:01:13 -0500

    finishplanet exercises

--- Program.cs
+++ Program.cs
@@ -7,7 +7,24 @@ namespace lists
     {
         static void Main(string[] args)
         {
-            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            List<string> lastPlanets = new List<string>() {"Neptune", "Uranus"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            planetList.AddRange(lastPlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+            planetList.ForEach(planet => Console.WriteLine(planet));
+            rockyPlanets.ForEach(planet => Console.WriteLine(planet));
+
         }
+
+        
     }
 }
